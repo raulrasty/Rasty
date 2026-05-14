053ead6149af5fb4767c7a5426196aa0efc3e237
ralvareztej <raul_rasty@hotmail.es>
2026-05-14 18:54:21 +0200

    arreglo render

--- server.js
+++ server.js
@@ -1,11 +1,3 @@
-const dns = require("dns");
-
-dns.setDefaultResultOrder("ipv4first");
-
-const https = require("https");
-
-https.globalAgent.options.family = 4;
-
 const express = require('express');
 require('dotenv').config();
 const path = require('path');
